03ef3d86660e36a1c813bf94bd3f60f70d9ca9b8
Richard W. Lincoln <r.w.lincoln@gmail.com>
2010-08-22 00:01:17 +0100

    Correcting copyright statement.

--- pypower/test.py
+++ pypower/test.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2010 Richard Lincoln <r.w.lincoln@gmail.com>
+# Copyright (C) 2009-2010 Richard Lincoln <r.w.lincoln@gmail.com>
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
