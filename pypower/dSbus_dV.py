478269f2ac22ec519418a71a21a0809eb3548330
Richard W. Lincoln <r.w.lincoln@gmail.com>
2010-08-22 10:10:44 +0100

    Adding dSbus_dV.

---
+++ pypower/dSbus_dV.py
@@ -0,0 +1,64 @@
+# Copyright (C) 1996-2010 Power System Engineering Research Center
+# Copyright (C) 2009-2010 Richard Lincoln <r.w.lincoln@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from numpy import conj
+from scipy.sparse import csr_matrix
+
+def dSbus_dV(Ybus, V):
+    """Computes partial derivatives of power injection w.r.t. voltage.
+
+    Returns two matrices containing partial derivatives of the complex bus
+    power injections w.r.t voltage magnitude and voltage angle respectively
+    (for all buses). If YBUS is a sparse matrix, the return values will be
+    also. The following explains the expressions used to form the matrices:
+
+    S = diag(V) * conj(Ibus) = diag(conj(Ibus)) * V
+
+    Partials of V & Ibus w.r.t. voltage magnitudes
+        dV/dVm = diag(V./abs(V))
+        dI/dVm = Ybus * dV/dVm = Ybus * diag(V./abs(V))
+
+    Partials of V & Ibus w.r.t. voltage angles
+        dV/dVa = j * diag(V)
+        dI/dVa = Ybus * dV/dVa = Ybus * j * diag(V)
+
+    Partials of S w.r.t. voltage magnitudes
+        dS/dVm = diag(V) * conj(dI/dVm) + diag(conj(Ibus)) * dV/dVm
+               = diag(V) * conj(Ybus * diag(V./abs(V)))
+                                        + conj(diag(Ibus)) * diag(V./abs(V))
+
+    Partials of S w.r.t. voltage angles
+        dS/dVa = diag(V) * conj(dI/dVa) + diag(conj(Ibus)) * dV/dVa
+               = diag(V) * conj(Ybus * j * diag(V))
+                                        + conj(diag(Ibus)) * j * diag(V)
+               = -j * diag(V) * conj(Ybus * diag(V))
+                                        + conj(diag(Ibus)) * j * diag(V)
+               = j * diag(V) * conj(diag(Ibus) - Ybus * diag(V))
+
+    @see: U{http://www.pserc.cornell.edu/matpower/}
+    @return: The partial derivatives of power injection w.r.t. voltage
+             magnitude and voltage angle.
+    """
+    ib = range(len(V))
+    I = Ybus * V
+
+    diagV = csr_matrix((V, (ib, ib)))
+    diagIbus = csr_matrix((I, (ib, ib)))
+    diagVnorm = csr_matrix((V / abs(V), (ib, ib)))
+
+    dS_dVm = diagV * conj(Ybus * diagVnorm) + conj(diagIbus) * diagVnorm
+    dS_dVa = 1j * diagV * conj(diagIbus - Ybus * diagV)
+
+    return dS_dVm, dS_dVa
