20df538538b0cad2daf540b4f7c6fe2d2d4f6fbd
Richard Lincoln <r.w.lincoln@gmail.com>
2010-12-29 19:14:54 +0000

    Adding case6ww, case9, case9Q and case14.

---
+++ pypower/case14.py
@@ -0,0 +1,109 @@
+# Copyright (C) 1996-2010 Power System Engineering Research Center (PSERC)
+# Copyright (C) 2010 Richard Lincoln <r.w.lincoln@gmail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA, USA
+
+from numpy import array
+
+def case14():
+    """Power flow data for IEEE 14 bus test case.
+    Please see L{caseformat} for details on the case file format.
+
+    This data was converted from IEEE Common Data Format
+    (ieee14cdf.txt) on 20-Sep-2004 by cdf2matp, rev. 1.11
+
+    Converted from IEEE CDF file from:
+        http://www.ee.washington.edu/research/pstca/
+
+    08/19/93 UW ARCHIVE           100.0  1962 W IEEE 14 Bus Test Case
+
+    @rtype: dict
+    @return: Power flow data for IEEE 14 bus test case.
+    @see: U{http://www.pserc.cornell.edu/matpower/}
+    """
+    ppc = {"version": '2'}
+
+    ##-----  Power Flow Data  -----##
+    ## system MVA base
+    ppc["baseMVA"] = 100.0
+
+    ## bus data
+    # bus_i type Pd Qd Gs Bs area Vm Va baseKV zone Vmax Vmin
+    ppc["bus"] = array([
+		[1,  3,  0,    0,   0, 0,  1, 1.06,    0,    0, 1, 1.06, 0.94],
+		[2,  2, 21.7, 12.7, 0, 0,  1, 1.045,  -4.98, 0, 1, 1.06, 0.94],
+		[3,  2, 94.2, 19,   0, 0,  1, 1.01,  -12.72, 0, 1, 1.06, 0.94],
+		[4,  1, 47.8, -3.9, 0, 0,  1, 1.019, -10.33, 0, 1, 1.06, 0.94],
+		[5,  1,  7.6,  1.6, 0, 0,  1, 1.02,   -8.78, 0, 1, 1.06, 0.94],
+		[6,  2, 11.2,  7.5, 0, 0,  1, 1.07,  -14.22, 0, 1, 1.06, 0.94],
+		[7,  1,  0,    0,   0, 0,  1, 1.062, -13.37, 0, 1, 1.06, 0.94],
+		[8,  2,  0,    0,   0, 0,  1, 1.09,  -13.36, 0, 1, 1.06, 0.94],
+		[9,  1, 29.5, 16.6, 0, 19, 1, 1.056, -14.94, 0, 1, 1.06, 0.94],
+		[10, 1,  9,    5.8, 0, 0,  1, 1.051, -15.1,  0, 1, 1.06, 0.94],
+		[11, 1,  3.5,  1.8, 0, 0,  1, 1.057, -14.79, 0, 1, 1.06, 0.94],
+		[12, 1,  6.1,  1.6, 0, 0,  1, 1.055, -15.07, 0, 1, 1.06, 0.94],
+		[13, 1, 13.5,  5.8, 0, 0,  1, 1.05,  -15.16, 0, 1, 1.06, 0.94],
+		[14, 1, 14.9,  5,   0, 0,  1, 1.036, -16.04, 0, 1, 1.06, 0.94]
+	])
+
+    ## generator data
+    # bus, Pg, Qg, Qmax, Qmin, Vg, mBase, status, Pmax, Pmin, Pc1, Pc2,
+    # Qc1min, Qc1max, Qc2min, Qc2max, ramp_agc, ramp_10, ramp_30, ramp_q, apf
+    ppc["gen"] = array([
+		[1, 232.4, -16.9, 10,   0, 1.06,  100, 1, 332.4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[2,  40,    42.4, 50, -40, 1.045, 100, 1, 140,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[3,   0,    23.4, 40,   0, 1.01,  100, 1, 100,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[6,   0,    12.2, 24,  -6, 1.07,  100, 1, 100,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[8,   0,    17.4, 24,  -6, 1.09,  100, 1, 100,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	])
+
+    ## branch data
+    # fbus, tbus, r, x, b, rateA, rateB, rateC, ratio, angle, status, angmin, angmax
+    ppc["branch"] = array([
+		[1,   2, 0.01938, 0.05917, 0.0528, 9900, 0, 0, 0,     0, 1, -360, 360],
+		[1,   5, 0.05403, 0.22304, 0.0492, 9900, 0, 0, 0,     0, 1, -360, 360],
+		[2,   3, 0.04699, 0.19797, 0.0438, 9900, 0, 0, 0,     0, 1, -360, 360],
+		[2,   4, 0.05811, 0.17632, 0.034,  9900, 0, 0, 0,     0, 1, -360, 360],
+		[2,   5, 0.05695, 0.17388, 0.0346, 9900, 0, 0, 0,     0, 1, -360, 360],
+		[3,   4, 0.06701, 0.17103, 0.0128, 9900, 0, 0, 0,     0, 1, -360, 360],
+		[4,   5, 0.01335, 0.04211, 0,      9900, 0, 0, 0,     0, 1, -360, 360],
+		[4,   7, 0,       0.20912, 0,      9900, 0, 0, 0.978, 0, 1, -360, 360],
+		[4,   9, 0,       0.55618, 0,      9900, 0, 0, 0.969, 0, 1, -360, 360],
+		[5,   6, 0,       0.25202, 0,      9900, 0, 0, 0.932, 0, 1, -360, 360],
+		[6,  11, 0.09498, 0.1989,  0,      9900, 0, 0, 0,     0, 1, -360, 360],
+		[6,  12, 0.12291, 0.25581, 0,      9900, 0, 0, 0,     0, 1, -360, 360],
+		[6,  13, 0.06615, 0.13027, 0,      9900, 0, 0, 0,     0, 1, -360, 360],
+		[7,   8, 0,       0.17615, 0,      9900, 0, 0, 0,     0, 1, -360, 360],
+		[7,   9, 0,       0.11001, 0,      9900, 0, 0, 0,     0, 1, -360, 360],
+		[9,  10, 0.03181, 0.0845,  0,      9900, 0, 0, 0,     0, 1, -360, 360],
+		[9,  14, 0.12711, 0.27038, 0,      9900, 0, 0, 0,     0, 1, -360, 360],
+		[10, 11, 0.08205, 0.19207, 0,      9900, 0, 0, 0,     0, 1, -360, 360],
+		[12, 13, 0.22092, 0.19988, 0,      9900, 0, 0, 0,     0, 1, -360, 360],
+		[13, 14, 0.17093, 0.34802, 0,      9900, 0, 0, 0,     0, 1, -360, 360]
+	])
+
+    ##-----  OPF Data  -----##
+    ## generator cost data
+    # 1 startup shutdown n x1 y1 ... xn yn
+    # 2 startup shutdown n c(n-1) ... c0
+    ppc["gencost"] = array([
+		[2, 0, 0, 3, 0.0430293, 20, 0],
+		[2, 0, 0, 3, 0.25,      20, 0],
+		[2, 0, 0, 3, 0.01,      40, 0],
+		[2, 0, 0, 3, 0.01,      40, 0],
+		[2, 0, 0, 3, 0.01,      40, 0]
+	])
+
+    return ppc
---
+++ pypower/case6ww.py
@@ -0,0 +1,84 @@
+# Copyright (C) 1996-2010 Power System Engineering Research Center (PSERC)
+# Copyright (C) 2010 Richard Lincoln <r.w.lincoln@gmail.com>
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA, USA
+
+from numpy import array
+
+def case6ww():
+    """Power flow data for 6 bus, 3 gen case from Wood & Wollenberg.
+    Please see L{caseformat} for details on the case file format.
+
+    This is the 6 bus example from pp. 104, 112, 119, 123-124, 549 of
+    "Power Generation, Operation, and Control, 2nd Edition",
+    by Allen. J. Wood and Bruce F. Wollenberg, John Wiley & Sons, NY, Jan 1996.
+
+    @rtype: dict
+    @return: Power flow data for 6 bus, 3 gen case from Wood & Wollenberg.
+    @see: U{http://www.pserc.cornell.edu/matpower/}
+    """
+    ppc = {"version": '2'}
+
+    ##-----  Power Flow Data  -----##
+    ## system MVA base
+    ppc["baseMVA"] = 100.0
+
+    ## bus data
+    # bus_i type Pd Qd Gs Bs area Vm Va baseKV zone Vmax Vmin
+    ppc["bus"] = array([
+		[1, 3,  0,  0, 0, 0, 1, 1.05, 0, 230, 1, 1.05, 1.05],
+		[2, 2,  0,  0, 0, 0, 1, 1.05, 0, 230, 1, 1.05, 1.05],
+		[3, 2,  0,  0, 0, 0, 1, 1.07, 0, 230, 1, 1.07, 1.07],
+		[4, 1, 70, 70, 0, 0, 1, 1,    0, 230, 1, 1.05, 0.95],
+		[5, 1, 70, 70, 0, 0, 1, 1,    0, 230, 1, 1.05, 0.95],
+		[6, 1, 70, 70, 0, 0, 1, 1,    0, 230, 1, 1.05, 0.95]
+	])
+
+    ## generator data
+    # bus, Pg, Qg, Qmax, Qmin, Vg, mBase, status, Pmax, Pmin, Pc1, Pc2,
+    # Qc1min, Qc1max, Qc2min, Qc2max, ramp_agc, ramp_10, ramp_30, ramp_q, apf
+    ppc["gen"] = array([
+		[1, 0,  0, 100, -100, 1.05, 100, 1, 200, 50,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[2, 50, 0, 100, -100, 1.05, 100, 1, 150, 37.5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+		[3, 60, 0, 100, -100, 1.07, 100, 1, 180, 45,   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+	])
+
+    ## branch data
+    # fbus, tbus, r, x, b, rateA, rateB, rateC, ratio, angle, status, angmin, angmax
+    ppc["branch"] = array([
+		[1, 2, 0.1,  0.2,  0.04, 40, 40, 40, 0, 0, 1, -360, 360],
+		[1, 4, 0.05, 0.2,  0.04, 60, 60, 60, 0, 0, 1, -360, 360],
+		[1, 5, 0.08, 0.3,  0.06, 40, 40, 40, 0, 0, 1, -360, 360],
+		[2, 3, 0.05, 0.25, 0.06, 40, 40, 40, 0, 0, 1, -360, 360],
+		[2, 4, 0.05, 0.1,  0.02, 60, 60, 60, 0, 0, 1, -360, 360],
+		[2, 5, 0.1,  0.3,  0.04, 30, 30, 30, 0, 0, 1, -360, 360],
+		[2, 6, 0.07, 0.2,  0.05, 90, 90, 90, 0, 0, 1, -360, 360],
+		[3, 5, 0.12, 0.26, 0.05, 70, 70, 70, 0, 0, 1, -360, 360],
+		[3, 6, 0.02, 0.1,  0.02, 80, 80, 80, 0, 0, 1, -360, 360],
+		[4, 5, 0.2,  0.4,  0.08, 20, 20, 20, 0, 0, 1, -360, 360],
+		[5, 6, 0.1,  0.3,  0.06, 40, 40, 40, 0, 0, 1, -360, 360]
+	])
+
+    ##-----  OPF Data  -----##
+    ## generator cost data
+    # 1 startup shutdown n x1 y1 ... xn yn
+    # 2 startup shutdown n c(n-1) ... c0
+    ppc["gencost"] = array([
+		[2, 0, 0, 3, 0.00533, 11.669, 213.1],
+		[2, 0, 0, 3, 0.00889, 10.333, 200],
+		[2, 0, 0, 3, 0.00741, 10.833, 240]
+	])
+
+    return ppc
