6c2100ead495684f8a415d9f0fdf11c837d7a227
Richard Lincoln <r.w.lincoln@gmail.com>
2010-12-30 01:28:50 +0000

    Adding bus degree function.

--- pypower/busorder.py
+++ pypower/busorder.py
@@ -14,39 +14,58 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA, USA
 
-from numpy import zeros, argsort
+from numpy import array, zeros, r_, argsort, flatnonzero as find
 
-def busorder(bus, branch, scheme=1):
-    """Reorders the bus matrix rows for improved Jabobian structure
-    according to the schemes described in the book 'Computational
-    Methods for Electric Power Systems' by Mariesa Crow.
+def busorder(bus, branch, scheme=0):
+    """Reorders the bus matrix rows for improved LU factorisation and forward/
+    backward substitution performance according to the schemes described in
+    Section 4.3 of the book 'Computational Methods for Electric Power Systems'
+    by Mariesa Crow.
     """
-    nb = bus.shape[0]
-    idx = bus[:, 0]
+    if scheme == 0:
+        # Order buses according to the number of branches connected to them.
+        degree = _degree(bus, branch)
+        idx = argsort(degree)
+    elif scheme == 1:
+        # Minimum degree/Markowitz/Tinney I algorithm. Updates the degree of
+        # buses after placing those with the lowest degree in the ordering
+        # scheme.
+        idx = array([], int)
+        degree = _degree(bus, branch)
+        elim = find(degree == min(degree))
+        idx = r_[idx, elim]
 
-    if scheme == 1:
-        nbrch = zeros(nb)
-
-        for b in range(nb):
-            i = bus[b, 0]
-            nbrch[b] += sum(branch[:, 0] == i)
-            nbrch[b] += sum(branch[:, 1] == i)
-
-        idx = argsort(nbrch)
+        print elim, idx
     elif scheme == 2:
-        pass
-    elif scheme == 3:
+        # Berry/Tinney II algorithm.
         raise NotImplementedError
     else:
         raise ValueError
 
-    bus = bus[:, idx]
+    print degree
+
+#    bus = bus[idx, :]
 
     return bus
 
 
+def _degree(bus, branch):
+    """Returns a vector of the number of branches in the given branch data
+    matrix connected to each bus in the given bus data matrix.
+    """
+    nb = bus.shape[0]
+
+    degree = zeros(nb)
+
+    for b in range(nb):
+        i = bus[b, 0]
+        degree[b] = sum(branch[:, 0] == i) + sum(branch[:, 1] == i)
+
+    return degree
+
+
 if __name__ == "__main__":
-    import case4gs
-    ppc = case4gs.case4gs()
-    bus = busorder(ppc["bus"], ppc["branch"])
+    import case6ww
+    ppc = case6ww.case6ww()
+    bus = busorder(ppc["bus"], ppc["branch"], scheme=1)
     print bus
