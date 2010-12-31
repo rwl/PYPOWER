3e04a7dfba8c48247b452f21c01745ed112f7293
Richard Lincoln <r.w.lincoln@gmail.com>
2010-12-31 00:02:12 +0000

    Implementing minimum degree algorithm.

--- pypower/busorder.py
+++ pypower/busorder.py
@@ -14,37 +14,51 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA, USA
 
-from numpy import array, zeros, r_, argsort, flatnonzero as find
+from numpy import array, zeros, r_, copy, argsort, Inf, flatnonzero as find
 
 def busorder(bus, branch, scheme=0):
     """Reorders the bus matrix rows for improved LU factorisation and forward/
     backward substitution performance according to the schemes described in
     Section 4.3 of the book 'Computational Methods for Electric Power Systems'
     by Mariesa Crow.
+
+    @param bus: bus data matrix
+    @param branch: branch data matrix
+    @param scheme: Ordering scheme. 1 - Buses are ordered according to the
+    number of branches connected to them. 2 - Minimum degree/Markowitz/Tinney I
+    algorithm.Updates the degree of buses after placing those with the lowest
+    degree in the ordering scheme and eliminating the associated branches.
+    @return: reordered bus data matrix
     """
     if scheme == 0:
-        # Order buses according to the number of branches connected to them.
         degree = _degree(bus, branch)
         idx = argsort(degree)
     elif scheme == 1:
-        # Minimum degree/Markowitz/Tinney I algorithm. Updates the degree of
-        # buses after placing those with the lowest degree in the ordering
-        # scheme.
-        idx = array([], int)
-        degree = _degree(bus, branch)
-        elim = find(degree == min(degree))
-        idx = r_[idx, elim]
+        # Eliminate branches from a copy of the given branch data.
+        brch = copy(branch)
 
-        print elim, idx
+        idx = array([], int)  # new bus indexes
+        elim = array([], int) # indexes of buses to be eliminated
+
+        while len(idx) < bus.shape[0]:
+            # Eliminate branches connected from or to buses already ordered.
+            il = zeros(brch.shape[0], bool)
+            for i in bus[:, 0][elim]:
+                il |= (brch[:, 0] == i) | (brch[:, 1] == i)
+            brch = brch[find(il == False), :]
+
+            degree = _degree(bus, brch)
+            # Ignore degrees of buses already ordered.
+            degree[idx] = Inf
+            elim = find(degree == min(degree))
+            idx = r_[idx, elim]
     elif scheme == 2:
         # Berry/Tinney II algorithm.
         raise NotImplementedError
     else:
         raise ValueError
 
-    print degree
-
-#    bus = bus[idx, :]
+    bus = bus[idx, :]
 
     return bus
 
@@ -57,9 +71,9 @@ def _degree(bus, branch):
 
     degree = zeros(nb)
 
-    for b in range(nb):
-        i = bus[b, 0]
-        degree[b] = sum(branch[:, 0] == i) + sum(branch[:, 1] == i)
+    for i in range(nb):
+        k = bus[i, 0]
+        degree[i] = sum(branch[:, 0] == k) + sum(branch[:, 1] == k)
 
     return degree
 
