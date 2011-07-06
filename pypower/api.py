2bf9f967b3b023e3d719411bbba700a7e9ebff60
Richard Lincoln <r.w.lincoln@gmail.com>
2011-07-06 14:56:52 +0100

    Adding api module.

---
+++ pypower/api.py
@@ -0,0 +1,120 @@
+# Copyright (C) 2011 Richard Lincoln
+#
+# PYPOWER is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published
+# by the Free Software Foundation, either version 3 of the License,
+# or (at your option) any later version.
+#
+# PYPOWER is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY], without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PYPOWER. If not, see <http://www.gnu.org/licenses/>.
+
+""" Pseudo-package for all of the core functions from PYPOWER.
+
+Use this module for importing PYPOWER function names into your namespace. For
+example::
+
+    from pypower.api import runpf
+"""
+
+from __future__ import absolute_import
+
+from .add_userfcn import add_userfcn
+from .bustypes import bustypes
+from .case118 import case118
+from .case14 import case14
+from .case24_ieee_rts import case24_ieee_rts
+from .case300 import case300
+from .case30pwl import case30pwl
+from .case30 import case30
+from .case30Q import case30Q
+from .case39 import case39
+from .case4gs import case4gs
+from .case57 import case57
+from .case6ww import case6ww
+from .case9 import case9
+from .case9Q import case9Q
+from .cplex_options import cplex_options
+from .d2AIbr_dV2 import d2AIbr_dV2
+from .d2ASbr_dV2 import d2ASbr_dV2
+from .d2Ibr_dV2 import d2Ibr_dV2
+from .d2Sbr_dV2 import d2Sbr_dV2
+from .d2Sbus_dV2 import d2Sbus_dV2
+from .dAbr_dV import dAbr_dV
+from .dcopf import dcopf
+from .dcopf_solver import dcopf_solver
+from .dcpf import dcpf
+from .dIbr_dV import dIbr_dV
+from .dSbr_dV import dSbr_dV
+from .dSbus_dV import dSbus_dV
+from .ext2int import ext2int
+from .fairmax import fairmax
+from .fdpf import fdpf
+from .gausspf import gausspf
+from .get_reorder import get_reorder
+from .hasPQcap import hasPQcap
+from .int2ext import int2ext
+from .ipoptopf_solver import ipoptopf_solver
+from .ipopt_options import ipopt_options
+from .isload import isload
+from .loadcase import loadcase
+from .makeAang import makeAang
+from .makeApq import makeApq
+from .makeAvl import makeAvl
+from .makeAy import makeAy
+from .makeBdc import makeBdc
+from .makeB import makeB
+from .makeLODF import makeLODF
+from .makePTDF import makePTDF
+from .makeSbus import makeSbus
+from .makeYbus import makeYbus
+from .modcost import modcost
+from .mosek_options import mosek_options
+from .newtonpf import newtonpf
+from .opf_args import opf_args
+from .opf_consfcn import opf_consfcn
+from .opf_costfcn import opf_costfcn
+from .opf_execute import opf_execute
+from .opf_hessfcn import opf_hessfcn
+from .opf_model import opf_model
+from .opf import opf
+from .opf_setup import opf_setup
+from .pfsoln import pfsoln
+from .pipsopf_solver import pipsopf_solver
+from .pips import pips
+from .pipsver import pipsver
+from .poly2pwl import poly2pwl
+from .polycost import polycost
+from .ppoption import ppoption
+from .ppver import ppver
+from .pqcost import pqcost
+from .printpf import printpf
+from .qps_cplex import qps_cplex
+from .qps_ipopt import qps_ipopt
+from .qps_mosek import qps_mosek
+from .qps_pips import qps_pips
+from .qps_pypower import qps_pypower
+from .remove_userfcn import remove_userfcn
+from .rundcopf import rundcopf
+from .rundcpf import rundcpf
+from .runduopf import runduopf
+from .runopf import runopf
+from .runopf_w_res import runopf_w_res
+from .runpf import runpf
+from .runuopf import runuopf
+from .run_userfcn import run_userfcn
+from .savecase import savecase
+from .scale_load import scale_load
+from .set_reorder import set_reorder
+from .toggle_iflims import toggle_iflims
+from .toggle_reserves import toggle_reserves
+from .total_load import total_load
+from .totcost import totcost
+from .uopf import uopf
+from .update_mupq import update_mupq
+
+from .t.test_pypower import test_pypower
