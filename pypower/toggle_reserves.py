c732f7b2e35bea69b87d66ff2b9d23cebaf7be2b
Richard Lincoln <r.w.lincoln@gmail.com>
2015-06-01 23:59:35 +0200

    Fixing copyright statements.

--- pypower/toggle_reserves.py
+++ pypower/toggle_reserves.py
@@ -1,4 +1,4 @@
-# Copyright 1996-2015 PSERC. All rights reserved.
+# Copyright (c) 1996-2015 PSERC. All rights reserved.
 # Use of this source code is governed by a BSD-style
 # license that can be found in the LICENSE file.
 from pypower.e2i_field import e2i_field
