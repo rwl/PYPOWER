373d9def6815b931f5dd8bf5a184bdbe961d2a8d
David P. Chassin <david.chassin@me.com>
2025-07-10 10:16:00 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='PYPOWER',
-    version='5.1.19a',
+    version='5.1.19',
     author='Richard Lincoln',
     author_email='r.w.lincoln@gmail.com',
     description='Solves power flow and optimal power flow problems',
